7541665b984cdb26eae39a680ed7b9c5a33f1f59
Ceres Rasmussen <sobkowowa@gmail.com>
2023-02-18 22:20:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('input', throttle(saveDataOnInput, 500));
+feedbackForm.addEventListener('submit', submitForm, { once: true });
+const dataForm = {};
 
+function saveDataOnInput() {
+  dataForm.email = feedbackForm.email.value;
+  dataForm.message = feedbackForm.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+function checkOnForm() {
+  if (localStorage) {
+    const localStorageData = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    feedbackForm.email.value = localStorageData.email;
+    feedbackForm.message.value = localStorageData.message;
+  }
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log(dataForm);
+  localStorage.clear();
+  feedbackForm.reset();
+}
+checkOnForm();
