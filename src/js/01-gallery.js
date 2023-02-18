3395c10ed52b4abb6816dfbddda8e005c4d40b41
Ceres Rasmussen <sobkowowa@gmail.com>
2023-02-18 19:41:34 +0200

    1-2 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryList = galleryItems.reduce(
+  (prew, next) =>
+    prew +
+    `<li><a class="gallery__item" href="${next.original}">
+  <img class="gallery__image" src="${next.preview}" alt="${next.description}" />
+</a></li>`,
+  ''
+);
+
+gallery.insertAdjacentHTML('afterbegin', galleryList);
+
+var lightBox = new SimpleLightbox('.gallery li .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(currentTimeOnPlay, 1000));
+
+function currentTimeOnPlay(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+// player.setCurrentTime(
+//     localStorage.getItem('videoplayer-current-time')
+//     ? localStorage.getItem('videoplayer-current-time')
+//     : 0
+//     );
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
