af5872b0cd0137680b98072818457cff708f3ee7
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-04-02 18:48:36 +0000

    Deploying to gh-pages from @ CeresRasmussen/goit-js-hw-08@ea59a0c852c5769bfc98cb61f639dd8763b9219f 🚀

---
+++ 03-feedback.c58ab4c3.js
@@ -0,0 +1,2 @@
+(function(e){if(""===e||void 0===e)return Promise.reject("Guest name must not be empty");Promise.resolve("Welcome ".concat(e))})("Mango").then((function(e){return console.log(e)})).catch((function(e){return console.error(e)}));
+//# sourceMappingURL=03-feedback.c58ab4c3.js.map
---
+++ 03-feedback.d44adcc7.js
@@ -0,0 +1,2 @@
+(e=>{if(""===e||void 0===e)return Promise.reject("Guest name must not be empty");Promise.resolve(`Welcome ${e}`)})("Mango").then((e=>console.log(e))).catch((e=>console.error(e)));
+//# sourceMappingURL=03-feedback.d44adcc7.js.map
